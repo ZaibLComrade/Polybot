0c11f13d62d8e48ffcc8d958ea3f18ea55e8fdcd
ZaibLComrade <kmjahanzaib@proton.me>
2025-02-09 17:01:54 +0600

    Added secret keys

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -76,7 +76,24 @@ class ApiService {
 
   // Retrieve the API key for each model from environment variables
   String _getApiKey(String modelName) {
-    return "";
+    switch (modelName) {
+      case 'gpt-4':
+        return const String.fromEnvironment('OPENAI_API_KEY',
+            defaultValue:
+                "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");
+      case 'gpt-3.5-turbo':
+        return const String.fromEnvironment('OPENAI_API_KEY');
+      case 'claude-2':
+        return const String.fromEnvironment('ANTHROPIC_API_KEY');
+      case 'deepseek-llm':
+        return const String.fromEnvironment('DEEPSEEK_API_KEY',
+            defaultValue: "sk-f1902595dc654ed78470b63797acac09");
+      case 'gemini-1.5-flash':
+        return const String.fromEnvironment('GEMINI_API_KEY',
+            defaultValue: "AIzaSyAFbt5CxNJVeSDqfPIaeFyBm9jMsN1Zo2A");
+      default:
+        throw Exception('API key not found for $modelName');
+    }
   }
 
   // Build the request body dynamically based on the model
